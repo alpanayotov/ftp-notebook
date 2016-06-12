9aa7b2add737837fd8b36cd7687f84791ac2a5c8
alpanayotov <alexander.panayotov@gmail.com>
2016-06-12 22:59:46 +0300

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+var config = {
+	apiKey: "AIzaSyDgkcplRfmnyH3ij85ln4eeUUDJquv7O4w",
+	authDomain: "flickering-torch-6807.firebaseapp.com",
+	databaseURL: "https://flickering-torch-6807.firebaseio.com",
+	storageBucket: "",
+};
+
+firebase.initializeApp( config );
---
+++ src/components/Application.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Application extends React.Component {
+	render() {
+		return <div className="container">Hello React!</div>;
+	}
+}
+
+export default Application;
