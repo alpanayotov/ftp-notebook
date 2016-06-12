2d236ae69ff9b2c94c8053f262695eceab902fad
Alexander Panayotov <alexander.panayotov@gmail.com>
2016-06-12 23:06:29 +0300

    Delete firebase-config.js
    It is added to .gitignore list

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-	apiKey: "AIzaSyDgkcplRfmnyH3ij85ln4eeUUDJquv7O4w",
-	authDomain: "flickering-torch-6807.firebaseapp.com",
-	databaseURL: "https://flickering-torch-6807.firebaseio.com",
-	storageBucket: "",
-};
-
-firebase.initializeApp( config );
